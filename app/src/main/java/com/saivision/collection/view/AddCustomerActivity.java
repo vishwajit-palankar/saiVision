c2eae9b33cf0b9f22a6ea729f2eafa846daee09c
vishwajit_palankar <vishwajit.palankar@gmail.com>
2016-08-28 16:34:37 +0530

    initial commit

---
+++ app/src/main/java/com/saivision/collection/view/AddCustomerActivity.java
@@ -0,0 +1,15 @@
+package com.saivision.collection.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.saivision.collection.R;
+
+public class AddCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
